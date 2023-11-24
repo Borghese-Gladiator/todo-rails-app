e5aaf9b57ab4d33ed158c3f1b177a9ed53e192ab
SW GA SUKI DA TO SAKABITAI <timothy_shee@hotmail.com>
2023-11-23 23:43:46 -0500

    chore: bump version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,5 @@
 # config/initializers/version.rb
 
 module YourAppName
-  VERSION = '0.1.1'.freeze # Replace '1.0.0' with your actual version
+  VERSION = '0.1.2'.freeze # Replace '1.0.0' with your actual version
 end
